9bfe40b1e57929fc7b1de22351ed3af3a8381f8b
Sam Graham <git@illusori.co.uk>
2011-08-22 09:01:03 +0100

    Add support for column numbers in gcc 4.5 error messages. See http://lists.gnu.org/archive/html/bug-gnu-emacs/2011-06/msg00310.html

--- flymake.el
+++ flymake.el
@@ -1004,7 +1004,10 @@ Convert it to flymake internal format."
      ;; PHP
      ("\\(?:Parse\\|Fatal\\) error: \\(.*\\) in \\(.*\\) on line \\([0-9]+\\)" 2 3 nil 1)
      ;; LaTeX warnings (fileless) ("\\(LaTeX \\(Warning\\|Error\\): .*\\) on input line \\([0-9]+\\)" 20 3 nil 1)
-     ;; ant/javac
+     ;; gcc after 4.5 (includes column number)
+     (" *\\(\\([a-zA-Z]:\\)?[^:(\t\n]+\\)\:\\([0-9]+\\)\:\\([0-9]+\\)\:[ \t\n]*\\(.+\\)"
+      2 3 4 5)
+     ;; ant/javac, also matches gcc prior to 4.5
      (" *\\(\\[javac\\] *\\)?\\(\\([a-zA-Z]:\\)?[^:(\t\n]+\\)\:\\([0-9]+\\)\:[ \t\n]*\\(.+\\)"
       2 4 nil 5))
    ;; compilation-error-regexp-alist)
